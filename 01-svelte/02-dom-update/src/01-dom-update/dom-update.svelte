2c35e67fe284c2c17c0bb13e4219b1a48a5d46d0
Braulio Diez Botella <braulio.diez@gmail.com>
2022-04-16 09:37:53 +0200

    dom update

--- 01-svelte/02-dom-update/src/01-dom-update/dom-update.svelte
+++ 01-svelte/02-dom-update/src/01-dom-update/dom-update.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+  let name = "";
+  let lastname = "";
 
+  console.log("Hey Rerender going on !!");
 </script>
 
-<h1>Place holder dom update demo</h1>
+<h1>Update name and lastname = name + lastname</h1>
+
+<input bind:value={name} />
+<input bind:value={lastname} />
+
+{name}&nbsp;{lastname}
