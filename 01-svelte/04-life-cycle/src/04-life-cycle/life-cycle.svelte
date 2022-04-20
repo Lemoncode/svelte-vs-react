d4c71ff96488dcc069f317714b23ce358f0bc3d8
Braulio Diez Botella <braulio.diez@gmail.com>
2022-04-20 18:50:18 +0200

    life cycle

--- 01-svelte/04-life-cycle/src/04-life-cycle/life-cycle.svelte
+++ 01-svelte/04-life-cycle/src/04-life-cycle/life-cycle.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
+  // Debounce example
+  // https://svelte.dev/repl/818b4b38293a4a67901473d6d4f60230?version=3.32.3
+  import { getMovies } from "./api";
+  import { Movie } from "./model";
+  let search = "";
+  let movies: Movie[] = [];
 
+  $: search &&
+    getMovies(search).then((result) => {
+      movies = result;
+    });
+
+  console.log(movies);
 </script>
 
 <h1>Place holder life cycle code demo</h1>
+
+<ul>
+  <input placeholder="move title" bind:value={search} />
+
+  {#each movies as movie}
+    <li>{movie.title}</li>
+  {/each}
+</ul>
