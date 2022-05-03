9bf1b987cfbcd212d6c1c95ff7bcc269c312b542
Braulio Diez Botella <braulio.diez@gmail.com>
2022-05-03 20:44:44 +0200

    reactive code

--- 00-react/src/03-reactive-code/index.tsx
+++ 00-react/src/03-reactive-code/index.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { getMovies } from "./api";
+import { Movie } from "./model";
 
 export const ReactiveCode = () => {
-  return <h1>03 Reactive Code</h1>;
+  const [search, setSearch] = React.useState("");
+  const [movies, setMovies] = React.useState<Movie[]>([]);
+
+  React.useEffect(() => {
+    getMovies(search).then(setMovies);
+  }, [search]);
+
+  return (
+    <>
+      <h1>03 Reactive Code</h1>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.title}>{movie.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
